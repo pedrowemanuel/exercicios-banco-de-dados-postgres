3ebd6800b1c99619260d56ac554cd8c8ee17b16d
Pedro Emanuel <pedronuel36@gmail.com>
2022-05-25 09:22:13 -0300

    Criação de novas tabelas

--- frota/create.sql
+++ frota/create.sql
@@ -50,15 +50,24 @@ CREATE TABLE eixo_veiculo (
 	id_pneu		INT 		NOT NULL 	REFERENCES pneu
 );
 
-CREATE TABLE vistoria (
+CREATE TABLE vistoria_veiculo (
     id              SERIAL      PRIMARY KEY,
-    data_cadastro   TIMESTAMP   NOT NULL,
+    observacao      VARCHAR(45),
     data_vistoria   TIMESTAMP   DEFAULT NULL,
     id_veiculo      INT         NOT NULL        REFERENCES veiculo,
     username        VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE medicoes_vistoria  (
+CREATE TABLE vistoria_pneu (
+    id             	 SERIAL      PRIMARY KEY,
+    data_cadastro  	 TIMESTAMP   NOT NULL,
+    data_vistoria  	 TIMESTAMP   DEFAULT NULL,
+    id_vistoria_veiculo  INT         NOT NULL        REFERENCES vistoria_veiculo,
+    username       	 VARCHAR(45) NOT NULL,
+    id_pneu              INT         NOT NULL        REFERENCES pneu
+);
+
+CREATE TABLE medicao_vistoria  (
     id                          SERIAL          PRIMARY KEY,
     raio                        INT             DEFAULT NULL,
     perfil                      VARCHAR(30)     DEFAULT NULL,
@@ -66,19 +75,26 @@ CREATE TABLE medicoes_vistoria  (
     indice_carga                VARCHAR(45)     DEFAULT NULL,
     indice_velocidade           VARCHAR(45)     DEFAULT NULL,
     id_marca_pneu               INT             NOT NULL        REFERENCES marca_pneu,
-    id_vistoria                 INT             NOT NULL        REFERENCES vistoria,
-    id_pneu                     INT             NOT NULL        REFERENCES pneu           
+    id_vistoria                 INT             NOT NULL        REFERENCES vistoria_pneu           
 );
 
 CREATE TABLE banda (
-    id          SERIAL      PRIMARY KEY,
-    descricao   VARCHAR(45) UNIQUE,
-    posicao     INT         NOT NULL
+    id          SERIAL      	PRIMARY KEY,
+    descricao   VARCHAR(45) 	UNIQUE		NOT NULL,
+    lado     	VARCHAR(45) 	NOT NULL
+);
+
+CREATE TABLE pneu_banda (
+    id          SERIAL     	 PRIMARY KEY,
+    observacao  VARCHAR(45),
+    id_pneu 	INT		 NOT NULL    REFERENCES pneu,
+    ordem	INT		 NOT NULL,
+    id_banda    INT		 NOT NULL    REFERENCES banda
 );
 
 CREATE TABLE medicao_banda_vistoria (
     id                    SERIAL      PRIMARY KEY,
     valor                 FLOAT       NOT NULL,
-    id_banda              INT         NOT NULL    REFERENCES banda,
-    id_medicao_vistoria   INT         NOT NULL    REFERENCES medicoes_vistoria
+    id_pneu_banda         INT         NOT NULL    REFERENCES pneu_banda,
+    id_medicao_vistoria   INT         NOT NULL    REFERENCES medicao_vistoria
 );
